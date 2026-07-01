f13736bea002644a4484b935df5e0bea481bf5fa
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-07-01 14:40:17 -0700

    feat: hell yeah

--- configuration.nix
+++ configuration.nix
@@ -2,122 +2,117 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries =
+      with pkgs;
+      [ fuse ]
+      ++ (pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs)
+      ++ (pkgs.appimageTools.defaultFhsEnvArgs.multiPkgs pkgs);
+  };
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  programs.appimage.enable = true;
 
-  # Configure network connections interactively with nmcli or nmtui.
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+
+  services.logind.settings.Login.HandleLidSwitch = "suspend";
+  services.logind.settings.Login.HandleLidSwitchDocked = "suspend";
+
+  boot.loader = {
+    grub.device = "nodev";
+    grub.enable = true;
+    efi.canTouchEfiVariables = false;
+    efi.efiSysMountPoint = "/boot";
+  };
+
+  services.chrony.enable = true;
+
+  services.envfs.enable = true;
+
+  services.resolved.enable = true;
+
+  boot.tmp.cleanOnBoot = true;
+
+  networking.hostName = "NCC-1701-D";
+
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  services.printing.enable = true;
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.alice = {
+  users.users.walterineils = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-    tree
-   ];
- };
+    extraGroups = [
+      "wheel"
+      "audio"
+      "networkmanager"
+      "sudo"
+      "docker"
+      "podman"
+      "dialout"
+    ];
+    shell = pkgs.unstable.fish;
+  };
 
-  # programs.firefox.enable = true;
+  hardware.graphics = {
+    enable = true;
+    package = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.mesa;
+    enable32Bit = true;
+    package32 =
+      inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.pkgsi686Linux.mesa;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+    ];
+    extraPackages32 = with pkgs; [
+      intel-vaapi-driver
+    ];
+  };
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  programs.firefox.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  environment.systemPackages = with pkgs; [
+    unstable.neovim
+    tmux
+    wget
+  ];
 
-  # List services that you want to enable:
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  networking.firewall = {
+    allowedTCPPorts = [ ];
+    allowedUDPPorts = [ ];
+  };
+  networking.firewall.enable = false;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  # DO NOT CHANGE THIS. For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
   system.stateVersion = "26.11"; # Did you read the comment?
-
 }
-
--- flake.nix
+++ flake.nix
@@ -1,16 +1,68 @@
 {
   inputs = {
-    # This is pointing to an unstable release.
-    # If you prefer a stable release instead, you can change the word unstable to the latest number shown here: https://nixos.org/download
-    # i.e. nixos-24.11
-    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    # NOTE: 'nixos' is the default hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    hyprland.url = "github:hyprwm/Hyprland?ref=v0.55.4";
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "unstable";
     };
   };
-}
+  outputs =
+    inputs@{
+      nixpkgs,
+      unstable,
+      nixos-hardware,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = import unstable {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      };
+      overlay-stable = final: prev: {
+        stable = nixpkgs.legacyPackages.${prev.system};
+      };
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs nixpkgs unstable; };
+        modules = [
+          ./configuration.nix
+          {
+            nix = {
+              channel.enable = false;
+              registry = {
+                nixpkgs.flake = nixpkgs;
+                unstable.flake = unstable;
+                nixos-hardware.flake = nixos-hardware;
+              };
+            };
 
+            nixpkgs.overlays = [
+              overlay-stable
+              overlay-unstable
+            ];
+
+            nix.nixPath = [
+              "nixpkgs=${nixpkgs.outPath}"
+              "unstable=${unstable.outPath}"
+            ];
+
+            nix.settings = {
+              substituters = [ "https://hyprland.cachix.org" ];
+              trusted-substituters = [ "https://hyprland.cachix.org" ];
+              trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+            };
+          }
+        ];
+      };
+    };
+}
