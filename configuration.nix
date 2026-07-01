bc448d1146e50a7a73b5a68090d997c886c90b5c
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-07-01 16:52:38 -0700

    feat: enable hyprland

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,14 @@
     ];
   };
 
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
