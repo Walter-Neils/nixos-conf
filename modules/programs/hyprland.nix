2b2216c76cf6f42b9f6d3a70a0903bac0f2adef8
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-02 12:21:56 -0700

    feat: update stub on system rebuild

--- modules/programs/hyprland.nix
+++ modules/programs/hyprland.nix
@@ -17,4 +17,19 @@
     portalPackage =
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
+
+
+system.userActivationScripts = {
+    updateHyprlandLuarc.text = ''
+      LUARC_PATH="$XDG_CONFIG_HOME/hypr/.luarc.json"
+      STUB_PATH="${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/share/hypr/stubs"
+      [[ ! -s "$LUARC_PATH" ]] && printf '{}' > "$LUARC_PATH"
+      cat <<< "$(${pkgs.jq}/bin/jq --arg new "$STUB_PATH" '
+        .workspace.library |= (. // [] | if any(endswith("/share/hypr/stubs"))
+            then map(if endswith("/share/hypr/stubs") then $new else . end)
+            else [$new] + .
+            end)
+      ' "$LUARC_PATH")" > "$LUARC_PATH"
+    '';
+  };
 }
