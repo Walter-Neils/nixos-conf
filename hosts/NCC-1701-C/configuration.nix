ccf69aa9516adda3af79421db769a1f6d486acc8
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-02 10:08:33 -0700

    feat: caelestia battery monitoring enabled

--- hosts/NCC-1701-C/configuration.nix
+++ hosts/NCC-1701-C/configuration.nix
@@ -13,6 +13,7 @@
 
   services.logind.settings.Login.HandleLidSwitch = "suspend";
   services.logind.settings.Login.HandleLidSwitchDocked = "suspend";
+  services.upower.enable = true;
 
   # DO NOT CHANGE THIS. For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
   system.stateVersion = "26.11"; # Did you read the comment?
