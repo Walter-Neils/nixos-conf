755a1cd9da3e3a0b7b0007216dc694caa89e543d
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-07-02 12:25:02 -0700

    refactor: remove unneeded caelestia wrapper and dedupe hyprland

--- modules/programs/caelestia.nix
+++ modules/programs/caelestia.nix
@@ -6,18 +6,16 @@
   ...
 }:
 
+let 
+  hyprland = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+  caelestia-shell = (inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
+    inherit hyprland;
+  });
+  in
 {
-  # 1. Enable the system daemon so the DBus interfaces exist
   services.power-profiles-daemon.enable = true;
 
-  # 2. Add the wrapped package to the system profile
   environment.systemPackages = [
-    (inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (oldAttrs: {
-      nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
-      postInstall = (oldAttrs.postInstall or "") + ''
-        wrapProgram $out/bin/caelestia-shell \
-          --prefix PATH : ${lib.makeBinPath [ pkgs.power-profiles-daemon ]}
-      '';
-    }))
+    caelestia-shell
   ];
 }
