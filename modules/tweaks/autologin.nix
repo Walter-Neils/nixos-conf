897831d2e183422d517ff6add6df4e68cbacde60
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-03 11:32:36 -0700

    chore: DRY

--- modules/tweaks/autologin.nix
+++ modules/tweaks/autologin.nix
@@ -1,15 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }:
+    let
+        start_hyprland = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/start-hyprland";
+{
 
   # 2. Configure greetd for autologin
   services.greetd = {
     enable = true;
     settings = {
       initial_session = {
-        command = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/start-hyprland";
+        command = "${start_hyprland}";
         user = "walterineils";
       };
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/start-hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${start_hyprland}";
         user = "greeter";
       };
     };
