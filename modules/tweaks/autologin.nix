f92794db1a2dcf1691efe34af2b85faa48f3bb91
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-07 11:34:04 -0700

    fix: autologin

--- modules/tweaks/autologin.nix
+++ modules/tweaks/autologin.nix
@@ -1,18 +1,16 @@
 { pkgs, inputs, ... }:
 {
-  config = {
-   services.greetd = {
-     enable = true;
-     settings = {
-       initial_session = {
-         command = config.win.autologin.command;
-         user = config.win.autologin.user;
-       };
-       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${config.win.autologin.command}";
-        user = "greeter";
-       };
-     };
-   };
-  };
+    services.greetd = {
+        enable = true;
+        settings = {
+            initial_session = {
+                command = config.win.autologin.command;
+                user = config.win.autologin.user;
+            };
+            default_session = {
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${config.win.autologin.command}";
+                user = "greeter";
+            };
+        };
+    };
 }
