00f089e7667060d3e4eac0c2e4364dcb21fb27a5
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-07 20:03:04 -0700

    wip: Kamikaze

---
+++ hosts/kamikaze/hardware-configuration.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, modulesPath, ... }:
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/mapper/root_vg-nixroot";
+      fsType = "btrfs";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/mapper/root_vg-home";
+      fsType = "btrfs";
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
