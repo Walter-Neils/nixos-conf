6055530302f1fd9a7b11835d4f578ea10916c05e
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-10 13:40:13 -0700

    feat: firefox memory limit

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -7,15 +7,25 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    firefox
-    (writeShellScriptBin "firefox" ''
-      exec ${systemd}/bin/systemd-run \
-        --user \
-        --scope \
-        --unit="firefox-limited" \
-        -p MemoryHigh=3.5G \
-        -p MemoryMax=4G \
-        ${firefox}/bin/firefox "$@"
-    '')
+    (pkgs.symlinkJoin {
+      name = "firefox-limited";
+      paths = [ pkgs.firefox ]; # Brings in desktop items, icons, and policies
+      postBuild = ''
+                rm $out/bin/firefox
+
+                cat << 'EOF' > $out/bin/firefox
+        #!/bin/sh
+        exec ${pkgs.systemd}/bin/systemd-run \
+          --user \
+          --scope \
+          --unit="firefox-limited" \
+          -p MemoryHigh=3.5G \
+          -p MemoryMax=4G \
+          ${pkgs.firefox}/bin/firefox "$@"
+        EOF
+
+                chmod +x $out/bin/firefox
+      '';
+    })
   ];
 }
