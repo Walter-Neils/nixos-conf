4ad0b354b44528a4e320fd420c5c9ae9add644a9
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-11 21:04:36 -0700

    feat: gsr-ui autostart

--- modules/programs/gpu-screen-recorder.nix
+++ modules/programs/gpu-screen-recorder.nix
@@ -5,6 +5,9 @@
   inputs,
   ...
 }:
+let
+  gsr-ui-pkg = inputs.gsr-ui.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder-ui;
+in
 {
   imports = [
     inputs.gsr-ui.nixosModules.default
@@ -14,5 +17,27 @@
     package = inputs.gsr-ui.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder;
     enable = true;
     ui.enable = true;
+  };
+
+  systemd.user.services.gpu-screen-recorder-ui = {
+    description = "GPU Screen Recorder UI (ShadowPlay Overlay)";
+
+    after = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+
+    environment = { };
+
+    serviceConfig = {
+      # Use the explicit package path we extracted above
+      ExecStart = "${gsr-ui-pkg}/bin/gsr-ui";
+
+      Environment = [
+        "PATH=${gsr-ui-pkg}/bin:/run/current-system/sw/bin"
+      ];
+
+      Restart = "on-failure";
+      RestartSec = "3";
+    };
   };
 }
