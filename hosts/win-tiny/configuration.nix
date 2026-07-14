6df471eb7ba03d8a71f7601d83e4ebee16202722
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-13 20:33:47 -0700

    feat: win-tiny

--- hosts/win-tiny/configuration.nix
+++ hosts/win-tiny/configuration.nix
@@ -9,14 +9,14 @@
   imports = [
     ../../modules/users/walterineils.nix
     ./hardware-configuration.nix
-    ../../modules/tweaks/autologin.nix
-    ../../modules/tweaks/auto-update.nix
-    ../../modules/program-groups/standard-gui.nix
-    ../../modules/program-groups/windows-compat.nix
-    ../../modules/program-groups/pro-audio.nix
-
-    ../../modules/programs/tailscale.nix
     ../../modules/programs/hyprland.nix
+    ../../modules/graphics/intel.nix
+    ../../modules/program-groups/standard-gui.nix
+    ../../modules/program-groups/pro-audio.nix
+    ../../modules/programs/tailscale.nix
+    ../../modules/programs/spotify.nix
+    ../../modules/programs/tidal.nix
+    ../../modules/tweaks/auto-update.nix
   ];
 
   # DO NOT CHANGE THIS. For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
