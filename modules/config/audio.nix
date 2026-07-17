1aa2209b3258cb5b58822bc3a999b258bd43795e
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-17 15:41:18 -0700

    wip

--- modules/config/audio.nix
+++ modules/config/audio.nix
@@ -10,7 +10,6 @@
   users.groups.pipewire = {};
   services.blueman.enable = true;
   security.rtkit.enable = true;
-  services.pipewire.systemWide = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -18,40 +17,7 @@
     pulse.enable = true;
     wireplumber = {
       enable = true;
-      extraConfig = {
-        "10-bluez" = {
-          "monitor.bluez.properties" = {
-            "bluez5.roles" = [
-              "a2dp_sink"
-              "a2dp_source"
-              "hfp_hf"
-              "hfp_ag"
-              "hsp_hs"
-              "hsp_ag"
-            ];
-          };
-          "wireplumber.profiles" = {
-            "main" = {
-              "monitor.bluez" = "required";
-            };
-          };
-        };
-      };
     };
   };
 
-  hardware.bluetooth.enable = true;
-  services.pipewire.wireplumber.extraConfig = {
-    "bluetooth" = {
-      "monitor.bluez.properties" = {
-        "bluez5.codecs" = [
-          "sbc"
-          "sbc_xq"
-          "aac"
-          "aptx"
-          "aptx_hd"
-        ];
-      };
-    };
-  };
 }
