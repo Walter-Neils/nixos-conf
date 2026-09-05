d22b4824ea0efb0fdafc5a55b4bdbea1f4fa6f79
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-09-04 21:50:35 -0700

    feat: protonvpn

--- hosts/kamikaze/configuration.nix
+++ hosts/kamikaze/configuration.nix
@@ -24,7 +24,6 @@
     ../../modules/programs/tailscale.nix
     ../../modules/programs/spotify.nix
     ../../modules/programs/kde-plasma.nix
-    ../../modules/programs/qbittorrent.nix
   ];
 
   # DO NOT CHANGE THIS. For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
--- modules/program-groups/vpn.nix
+++ modules/program-groups/vpn.nix
@@ -8,5 +8,6 @@
 {
   imports = [
     ../programs/protonvpn.nix
+    ../programs/qbittorrent.nix
   ];
 }
